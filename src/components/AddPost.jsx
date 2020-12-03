729cfc4531214dee986bc953a71ef5c44c4884cd
Ian Hansson <ian.hansson@gmail.com>
2020-12-03 04:46:43 +0000

    User sign up

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {firestore} from "../firebase";
+import {auth} from "../firebase";
 
 class AddPost extends Component {
   state = { title: '', content: '' };
@@ -13,15 +14,16 @@ class AddPost extends Component {
     event.preventDefault();
 
     const { title, content } = this.state;
+    const { uid, displayName, email, photoURL} = auth.currentUser || {}
 
     const post = {
       title,
       content,
       user: {
-        uid: '1111',
-        displayName: 'Steve Kinney',
-        email: 'steve@mailinator.com',
-        photoURL: 'http://placekitten.com/g/200/200',
+        uid,
+        displayName,
+        email,
+        photoURL,
       },
       favorites: 0,
       comments: 0,
@@ -31,6 +33,9 @@ class AddPost extends Component {
     firestore.collection('posts').add(post)
 
     this.setState({ title: '', content: '' });
+
+    if(!uid) return null;
+
   };
 
   render() {
--- src/firebase.js
+++ src/firebase.js
@@ -27,4 +27,39 @@ firestore.settings({ timestampsInSnapshots: true })
 
 window.firebase = firebase
 
+export const createUserProfileDocument = async (user, additionalData) => {
+    if (!user) return;
+
+    const userRef = firestore.doc(`users/${user.uid}`)
+
+    const snapshot = await userRef.get()
+
+    if(!snapshot.exists){
+        const { displayName, email, photoURL } = user
+        const createdAt = new Date()
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                photoURL,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.error(error)
+        }
+    }
+
+}
+
+export const getUserDocument = async (uid) => {
+    if(!uid) return null
+    try {
+        const userDocument = await firestore.collection('users').doc(uid).get()
+        return { uid, ...userDocument.data() }
+    } catch(error){
+        console.error(error)
+    }
+}
+
 export default firebase
